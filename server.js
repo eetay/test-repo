9888af49b3ff9e251f9fa305edcbdef54bc9b10d
Eetay Natan <eetay.natan@samanage.com>
2016-03-20 09:48:51 +0200

    Hello World

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-var express = require('express'); 
-var app = express(); 
-app.get('/', function(req, res){ 
-  res.send('Hello World'); 
+var express = require('express');
+var app = express();
+var options = {
+  root: __dirname + '/public/',
+}
+var errorFunc = function(err) {
+  if (err) console.log('err')
+}
+
+app.get('/', function(req, res){
+  res.sendFile('index.html', options, errorFunc);
+});
+
+app.get('/*', function(req, res){
+  res.sendFile(req.url, options, errorFunc);
 });
 app.listen(3000);
